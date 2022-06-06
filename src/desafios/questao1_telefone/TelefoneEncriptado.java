9ad93e0583ca5e2309b34e45fe84a35bb1b0d8e8
MarinaRuanna <marina_ruana@hotmail.com>
2022-06-05 21:37:39 -0300

    .

--- src/desafios/questao1_telefone/TelefoneEncriptado.java
+++ src/desafios/questao1_telefone/TelefoneEncriptado.java
@@ -16,25 +16,25 @@ public class TelefoneEncriptado {
         for (int i = 0; i < expressao.length(); i++) {
             char c = expressao.charAt(i);
             if( c == 'A' || c == 'B' || c == 'C') {
-                System.out.println(2);
+                System.out.print(2);
             } else if (c == 'D' || c == 'E' || c == 'F') {
-                System.out.println(3);
+                System.out.print(3);
             } else if (c == 'G' || c == 'H' || c == 'I') {
-                System.out.println(4);
+                System.out.print(4);
             }else if (c == 'J' || c == 'K' || c == 'L') {
-                System.out.println(5);
+                System.out.print(5);
             }else if (c == 'M' || c == 'N' || c == 'O') {
-                System.out.println(6);
+                System.out.print(6);
             }else if (c == 'P' || c == 'Q' || c == 'R' || c == 'S') {
-                System.out.println(7);
+                System.out.print(7);
             }else if (c == 'T' || c == 'U' || c == 'V') {
-                System.out.println(8);
-            }else if (c == 0) {
-                System.out.println(0);
-            }else if (c == 1) {
-                System.out.println(0);
+                System.out.print(8);
+            }else if (c == '0') {
+                System.out.print(0);
+            }else if (c == '1') {
+                System.out.print(1);
             }else if (c == '-') {
-                System.out.println('-');
+                System.out.print('-');
             }
         }
 
